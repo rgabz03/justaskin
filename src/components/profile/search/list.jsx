2ebdea593799c844567a1511911c1bf331b7060f
rgabz03@gmail.com <rgabz03@gmail.com>
2021-03-06 20:40:47 +0800

    added new design for search profile

--- src/components/profile/search/list.jsx
+++ src/components/profile/search/list.jsx
@@ -33,6 +33,31 @@ export default class List extends Component {
                         <span className="fa fa-search form-control-feedback"></span>
                         <input type="text" className="form-control" placeholder="Search"/>
                     </div>
+
+
+
+
+                    
+
+
+                    <div className="justify-content-center">
+                            <div className="card p-3">
+                                <div className="d-flex align-items-center">
+                                    <div className="image"> <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" className="rounded" width="155"/> </div>
+                                    <div className="ml-3 w-100">
+                                        <h5 className="mb-0 mt-0">Alex Morrision</h5> <span>Senior Journalist</span>
+                                        <div className="p-2 mt-2 bg-primary-custom d-flex justify-content-between rounded text-white stats">
+                                            <div className="d-flex flex-column"> <span className="articles">Post</span> <span className="number1">38</span> </div>
+                                            <div className="d-flex flex-column"> <span className="followers">Followers</span> <span className="number2">980</span> </div>
+                                        </div>
+                                        <div className="button mt-2 d-flex flex-row align-items-center"> <button className="btn btn-sm btn-outline-primary-custom w-100"><i className="fa fa-comment-o"></i></button> <button className="btn btn-sm btn-primary-custom w-100 ml-2">Follow</button> </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        
                     <div className="contianer">
                         
                         <div className="row border-bottom shadow-sm p-2 bg-white">
@@ -45,7 +70,7 @@ export default class List extends Component {
                                     <h5 className="mb-1">User Name</h5>
                                     <small>Followers 13</small>
                                 </div>
-                                <p className="mb-1"><span class="badge badge-secondary">Website Developer</span></p>
+                                <p className="mb-1"><span className="badge badge-secondary">Website Developer</span></p>
 
                                 <div className="d-flex justify-content-between bd-highlight float-right">
                                     <div className="d-flex justify-content-start mb-2">
@@ -68,7 +93,7 @@ export default class List extends Component {
                                     <h5 className="mb-1">User Name</h5>
                                     <small>Followers 13</small>
                                 </div>
-                                <p className="mb-1"><span class="badge badge-secondary">Website Developer</span></p>
+                                <p className="mb-1"><span className="badge badge-secondary">Website Developer</span></p>
 
                                 <div className="d-flex justify-content-between bd-highlight float-right">
                                     <div className="d-flex justify-content-start mb-2">
@@ -78,30 +103,9 @@ export default class List extends Component {
                                 </div>
                             </div>
                         </div>
-                        {/* <div class="col">
-                            <div class="card">
-                                <img src="https://www.w3schools.com/bootstrap4/img_avatar3.png" class="card-img-top" alt="..."/>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card">
-                                <img src="https://www.w3schools.com/bootstrap4/img_avatar3.png" class="card-img-top" alt="..."/>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card">
-                                <img src="https://www.w3schools.com/bootstrap4/img_avatar3.png" class="card-img-top" alt="..."/>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                </div>
-                            </div>
-                        </div> */}
+
+
+
                         
                     </div>
                 </div>
